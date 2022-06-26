30780605e08b2063392b55791e1fc90ac4d64609
feddericahu99 <federica.hu339@gmail.com>
2022-06-26 22:09:22 +0200

    before BONUS

--- js/script.js
+++ js/script.js
@@ -22,9 +22,30 @@ for (let i = 0; i < emailList.length; i++) {
 }
 
 /* mostro in console l'esito */
-
 if (flag == false) {
     console.log(email + ': accesso non autorizzato')
 } else {
     console.log(email + ': accesso autorizzato')
+}
+
+/* Dadi  
+1-Creo due generatori casuli di numeri da 1 a 6.
+2-Assegno i numeri a due player.
+3-Chi ha il numero più alto otterrà la vittoria.
+3-Porto gli lementi nel Dom
+*/
+
+
+// Creo generatori casuali
+bot = Math.floor (Math.random(1 , 6) * 6);
+player = Math.floor (Math.random(1 , 6) * 6);
+console.log(bot, player);
+
+// Dichiaro il vincitore o il pareggio 
+if(bot < player) {
+    document.getElementById('winner').innerText = 'Hai vinto!'
+} else if (player < bot) {
+    document.getElementById('winner').innerText = 'Hai perso!'
+} else {
+    document.getElementById('winner').innerText = 'Pareggio!'
 }
